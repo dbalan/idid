0e4432e7b756294592838c3c9738836791265c47
Dhananjay Balan <dhananjay@cliqz.com>
2018-04-06 16:05:50 +0200

    Moved command parsing to a seperate library.

--- app/Main.hs
+++ app/Main.hs
@@ -1,67 +1,9 @@
 module Main where
 
-import Options.Applicative
-import Data.Monoid
-import Control.Applicative
-
--- definitions for commands
-data Command = CommandNew
-             | CommandLast {
-                 period :: Period
-                 }
-               deriving (Show)
-
-data Period = Day
-  | Week
-  | Month
-  deriving (Show)
-
--- parsers for command
-parseCommand :: Parser Command
-parseCommand = subparser $
-  (command
-   "new"
-    (info (helper <*> parseNewCommand)
-     (fullDesc <> progDesc "new idid entry"))
-  ) <>
-
-  (command
-   "last"
-   (info (helper <*> parseWhatCommand)
-    (fullDesc <> progDesc "list what I did"))
-  )
-
-parseNewCommand :: Parser Command
-parseNewCommand = pure (CommandNew)
-
-parseWhatCommand :: Parser Command
-parseWhatCommand = CommandLast <$> periodParser
-
-periodParser :: Parser Period
-periodParser = subparser $
-  (command
-  "day"
-  (info (helper <*> pure (Day))
-        (fullDesc <> progDesc "last day"))
-  ) <>
-  (command
-  "week"
-  (info (helper <*> pure (Week))
-        (fullDesc <> progDesc "last day"))
-  ) <>
-  (command
-  "month"
-  (info (helper <*> pure (Month))
-        (fullDesc <> progDesc "last day"))
-  )
-
-
-showHelpOnErrorExecParser :: ParserInfo a -> IO a
-showHelpOnErrorExecParser = customExecParser (prefs showHelpOnError)
+import Cli
 
 main :: IO ()
 main = do
-  cmd <- showHelpOnErrorExecParser (info (helper <*> parseCommand)
-                                    (fullDesc <> progDesc "description" <> header "header"))
+  cmd <- customIdidParser
   putStrLn $ show cmd
 
---
+++ src/Cli.hs
@@ -0,0 +1,74 @@
+module Cli
+  ( customIdidParser
+  , Command
+  , Period
+  ) where
+
+import Options.Applicative
+import Control.Applicative
+import Data.Monoid
+
+-- definitions for commands
+data Command = CommandNew
+             | CommandWhat {
+                 period :: Period
+                 }
+               deriving (Show)
+
+data Period = Day
+  | Week
+  | Month
+  deriving (Show)
+
+-- description
+hdr :: String
+hdr = "I did what?"
+
+desc :: String
+desc = "I did what is a simple CLI to track things that you do, \
+       \ the program has command, one to record a small msg what you did \
+       \ and one to list all the things you did for given last period"
+
+customIdidParser :: IO Command
+customIdidParser = customExecParser (prefs showHelpOnError) $ (info (helper <*> parseCommand)
+                                                              (fullDesc <> progDesc desc <> header hdr))
+
+-- parsers for command
+parseCommand :: Parser Command
+parseCommand = subparser $
+  (command
+   "new"
+    (info (helper <*> parseNewCommand)
+     (fullDesc <> progDesc "new idid entry"))
+  ) <>
+
+  (command
+   "last"
+   (info (helper <*> parseWhatCommand)
+    (fullDesc <> progDesc "list what I did"))
+  )
+
+parseNewCommand :: Parser Command
+parseNewCommand = pure (CommandNew)
+
+parseWhatCommand :: Parser Command
+parseWhatCommand = CommandWhat <$> periodParser
+
+periodParser :: Parser Period
+periodParser = subparser $
+  (command
+  "lastday"
+  (info (helper <*> pure (Day))
+        (fullDesc <> progDesc "last day"))
+  ) <>
+  (command
+  "lastweek"
+  (info (helper <*> pure (Week))
+        (fullDesc <> progDesc "last week"))
+  ) <>
+  (command
+  "lastmonth"
+  (info (helper <*> pure (Month))
+        (fullDesc <> progDesc "last month"))
+  )
+
